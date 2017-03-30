cec8e44cf6c36c4738f6a3410bd3ac4116f573be
Sudhir Nallam <psn240@nyu.edu>
2017-03-30 12:41:00 -0400

    removing unnessary files

--- LanuageModel/run_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'Start Run'
-python main.py --epochs 6 --cuda --name sample > output_ptb/sample.txt
-#python main.py --epochs 50 --cuda --name base_LSTM_50 > output_ptb/output_LSTM_50.txt
-#python main.py --epochs 50 --cuda --model GRU --name base_GRU_50 > output_ptb/output_GRU_50.txt
-echo 'Done!!'
